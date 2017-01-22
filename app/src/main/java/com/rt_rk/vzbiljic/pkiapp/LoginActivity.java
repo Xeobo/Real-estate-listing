76faa4d92c5741523ba04934c75534fb9efcbead
Vladimir Zbiljic <vladimir.zbiljic@gmail.com>
2017-01-22 23:43:15 +0100

    Main activty base added

--- app/src/main/java/com/rt_rk/vzbiljic/pkiapp/LoginActivity.java
+++ app/src/main/java/com/rt_rk/vzbiljic/pkiapp/LoginActivity.java
@@ -1,26 +1,40 @@
 package com.rt_rk.vzbiljic.pkiapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AbstractActivity implements View.OnFocusChangeListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreateBase(Bundle savedInstanceState){
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        actionBar.setIcon(R.drawable.homeicon);
         setContentView(R.layout.activity_login);
 
-
         final Button button = (Button) findViewById(R.id.button);
+
+        final EditText username = (EditText) findViewById(R.id.username);
+        username.setOnFocusChangeListener(this);
+
+        final EditText password = (EditText) findViewById(R.id.password);
+        password.setOnFocusChangeListener(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,11 +43,30 @@ public class LoginActivity extends Activity {
                 EditText password = (EditText) findViewById(R.id.password);
                 if("admin".equals(username.getText().toString()) && "admin".equals(password.getText().toString()) ) {
                     Toast.makeText(LoginActivity.this, "Welcome admin!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                    startActivity(intent);
                 }else if("user".equals(username.getText().toString()) && "user".equals(password.getText().toString())){
                     Toast.makeText(LoginActivity.this, "Welcome user!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginActivity.this, "Wrong credentials", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+    }
+
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+        if(!hasFocus && (view.getId() == R.id.username || view.getId() == R.id.password)) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+//        }else if(hasFocus && (view.getId() == R.id.username || view.getId() == R.id.password)){
+//            InputMethodManager imm =  (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//            imm.showSoftInput(view,0);
+//        }
     }
 }
---
+++ app/src/main/java/com/rt_rk/vzbiljic/pkiapp/MainActivity.java
@@ -0,0 +1,12 @@
+package com.rt_rk.vzbiljic.pkiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AbstractActivity {
+
+    @Override
+    protected void onCreateBase(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+    }
+}
